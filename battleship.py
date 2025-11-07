c2574dc7d63b7166050674e93acdd8b8a58dbf87
agnesrausz <rauszka94@gmail.com>
2025-11-07 23:56:31 +0100

    implement init_board

---
+++ battleship.py
@@ -0,0 +1,9 @@
+
+def init_board(board_rows=5, board_cols=5):
+    board = []
+    for _ in range(board_rows):
+        board.append(['0'] * board_cols)
+    return board
+
+if __name__ == "__main__":
+    pass
