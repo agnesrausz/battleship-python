9d84d3078f680909f86efaaa676b77378fa24a73
agnesrausz <rauszka94@gmail.com>
2025-11-12 01:32:43 +0100

    implement get_ship_placement

--- battleship.py
+++ battleship.py
@@ -12,6 +12,53 @@ def place_ship(board, row, col, direction, ship_size):
 
 def get_ship_placement(board, player):
     pass
+def get_ship_placement(board, ship_size):
+    """Returns the coordinates of a valid ship placement on board."""
+    first_letter_ascii_code = ord('a')
+    row_len = len(board)
+    col_len = len(board[0])
+
+    while True:
+        if ship_size == 1:
+            placement = input(
+                f'Enter your ship placement (e.g. B2): ').strip().lower().split()
+            placement.append('h')  # default direction for size 1 ship
+        else:
+            placement = input(
+                f'Enter your ship placement (e.g. B2 H for horizontal or C3 V for vertical): ').strip().lower().split()
+
+        if len(placement[0]) != 2:
+            print('Invalid coordinate, try again!')
+            continue
+
+        if not placement[0][0].isalpha() or not placement[0][1].isdigit():
+            print('Invalid coordinate, try again!')
+            continue
+
+        if len(placement) != 2:
+            print('Invalid input!')
+            continue
+
+        row = ord(placement[0][0]) - first_letter_ascii_code
+        col = int(placement[0][1]) - 1
+        direction = placement[1][0]
+
+        if direction not in ['h', 'v']:
+            print('Invalid direction, try again!')
+            continue
+
+        if not (0 <= row < row_len and 0 <= col < col_len):
+            print('That move is out of bounds, try again!')
+            continue
+
+        coordinates = []
+        for offset in range(ship_size):
+            if direction == 'h':
+                coordinates.append((row, col + offset))
+            else:
+                coordinates.append((row + offset, col))
+
+        return coordinates
 
 
 if __name__ == "__main__":
